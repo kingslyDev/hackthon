c60e38879b7fac92f750266db99bd1aa846dbc9e
ARVAZAKI <arvazf09@gmail.com>
2024-12-08 14:08:40 +0700

    setup

--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,10 @@
-const express = require('express');
-const app = express();
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+require("dotenv").config();
 
+app.use(bodyParser.json());
+app.use(cors());
 app.listen(8080, () => {
       console.log(`app running http://localhost:8080`)
 })
