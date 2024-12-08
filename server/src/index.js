79674fa678485a79437cd062e55619fe792bca1f
ARVAZAKI <arvazf09@gmail.com>
2024-12-08 14:10:16 +0700

    update setup

--- server/src/index.js
+++ server/src/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 require("dotenv").config();
-
+const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 app.listen(8080, () => {
